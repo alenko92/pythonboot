d3964f62f1f2683e96137a63a9eb362814e94a6c
Alex Sanko <alenko_92@hotmail.com>
2020-06-01 14:03:34 -0400

    Update statementtest.py

--- statementtest.py
+++ statementtest.py
@@ -7,3 +7,7 @@ for _ in splitted:
     if 's' in _[0] and len(_) > 2:
         print(_)
 
+# Second: Use range() to print all the even numbers from 0 to 10 
+for _ in range(0, 10):
+    if _%2 == 0:
+        print(_)
