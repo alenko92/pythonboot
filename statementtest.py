536fb620e7a3c413509e78615ae0fd28b6059ad3
Alex Sanko <alenko_92@hotmail.com>
2020-06-01 14:04:16 -0400

    Update statementtest.py

--- statementtest.py
+++ statementtest.py
@@ -23,3 +23,15 @@ for _ in splitted:
     if len(_)% 2 == 0:
         print(_)
 
+# Fifth: Write a program that prints the integers from 1 to 100. But for multiples of three print "Fizz"
+#       instead of the number and for the multiples of five print "Buzz". For numbers which are multiples 
+#       of both three and five print "FizzBuzz"
+for _ in range(1, 100):
+    if _ % 3 == 0 and _ % 5 == 0:
+        print('FizzBuzz')
+    elif _ % 3 == 0:
+        print('Fizz')
+    elif _ % 5 == 0:
+        print('Buzz')
+    else:
+        print(_)
