b447fbf7ccae0c28e3a6caf967eb88f7aa2abe01
Alex Sanko <alenko_92@hotmail.com>
2020-06-02 13:17:52 -0400

    Update piglatin.py

--- piglatin.py
+++ piglatin.py
@@ -4,7 +4,12 @@
 # eg. word --> ordway
 # eg. apple --> appleway
 
-def piglatin(aword):
-    return aword
+def piglatin(aword = 'n/a'):
+    if (aword[0] == 'a' or aword[0] == 'e' or aword[0] == 'i' or aword[0] == 'o' or aword[0] =='u'):
+        return aword + 'ay'
+    else:
+        return aword[1:] + aword[0] +'ay'
 
 
+print(piglatin('word'))
+print(piglatin('apple'))
