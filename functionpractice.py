a6b568c8391e828416d8b3b659c82e1865a26da9
Alex Sanko <alenko_92@hotmail.com>
2020-06-02 14:58:24 -0400

    Update functionpractice.py

--- functionpractice.py
+++ functionpractice.py
@@ -1,7 +1,14 @@
 # Warmup
 
 def lesser_of_two_evens(a, b):
-    if (a + b)%2 != 0:
+    '''
+    DOCSTRING:  This function returns the greater even number
+    INPUT: integers 'a' and 'b'
+    OUTPUT: 'a' or 'b' or 'same number' if both numbers are equal
+    '''
+    if a == b:
+        return 'same number'
+    elif (a + b)%2 != 0:
         if a%2 == 0:
             return a
         else:
@@ -12,5 +19,19 @@ def lesser_of_two_evens(a, b):
         else: 
             return b
 
-print(lesser_of_two_evens(2, 4))
-print(lesser_of_two_evens(2, 5))
+
+def animal_crackers(text):
+    '''
+    DOCSTRING:  This function returns True if both words begin with the same letter
+    INPUT: any two word string
+    OUTPUT: 'True' or 'False'
+    '''
+    splitted = text.split(' ')
+    if splitted[0][0] == splitted[1][0]:
+        return True
+    else: 
+        return False
+
+
+print(animal_crackers('Levelheaded Llama'))
+print(animal_crackers('Crazy Kangaroo'))
