96ce810947d30955503d4aeb7d95bfe30e7d34df
Alex Sanko <alenko_92@hotmail.com>
2020-06-02 13:22:13 -0400

    Update piglatin.py

--- piglatin.py
+++ piglatin.py
@@ -1,10 +1,16 @@
 # PIG LATIN
-# If a word starts witha  vowel, add 'ay' to the end
+# If a word starts with a  vowel, add 'ay' to the end
 # If a word doesn't start with a vowel, put the first letter at the end and add 'ay'
 # eg. word --> ordway
 # eg. apple --> appleway
 
 def piglatin(aword = 'n/a'):
+    '''
+    DOCSTRING:  This function returns the inputed string + 'ay' at the end if it begins with a vowel, 
+                and puts the first letter at the end and then adds 'ay'
+    INPUT: a string
+    OUTPUT: string + ay
+    '''
     if (aword[0] == 'a' or aword[0] == 'e' or aword[0] == 'i' or aword[0] == 'o' or aword[0] =='u'):
         return aword + 'ay'
     else:
