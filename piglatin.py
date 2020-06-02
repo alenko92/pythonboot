a9c33c4f64c10829437821704c1e77c466882129
Alex Sanko <alenko_92@hotmail.com>
2020-06-02 13:07:12 -0400

    Create piglatin.py

---
+++ piglatin.py
@@ -0,0 +1,10 @@
+# PIG LATIN
+# If a word starts witha  vowel, add 'ay' to the end
+# If a word doesn't start with a vowel, put the first letter at the end and add 'ay'
+# eg. word --> ordway
+# eg. apple --> appleway
+
+def piglatin(aword):
+    return aword
+
+
